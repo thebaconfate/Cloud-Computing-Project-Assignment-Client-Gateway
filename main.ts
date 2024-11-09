b603f9dfaa295cf3bd09fa1a28d8665059ce0c71
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-11-09 21:15:41 +0100

    created pipeline part

---
+++ main.ts
@@ -0,0 +1,72 @@
+import Fastify, { FastifyInstance } from "fastify";
+import S from "fluent-json-schema";
+import { Observable, Subject } from "rxjs";
+
+interface Order {
+  user_id: number;
+  timestamp_ns: number;
+  price: number;
+  symbol: string;
+  quantity: number;
+  order_type: string;
+  trader_type: string;
+}
+
+const fastify: FastifyInstance = Fastify();
+
+const schema = S.object()
+  .prop("user_id", S.integer().minimum(0).required())
+  .prop("timestamp_ns", S.integer().minimum(0).required())
+  .prop("price", S.number().minimum(0).required())
+  .prop("symbol", S.string().minLength(1).required())
+  .prop("quantity", S.integer().minimum(1).required())
+  .prop("order_type", S.string().enum(["bid", "ask"]).required())
+  .prop("trader_type", S.string().minLength(1).required());
+
+let startTime: null | Date = null;
+const counterObservable = new Subject<number>();
+let counter = 0;
+const second = 1000;
+const observable = new Subject();
+observable.subscribe((rawData) => {
+  const data = rawData as Order;
+  counter++;
+  counterObservable.next(counter);
+});
+
+const interval = setInterval(() => {
+  if (startTime && counter % 1077986 === 0) {
+    const now = new Date();
+    console.log("Start time: " + startTime.toString());
+    console.log("Counter: " + counter);
+    console.log("Current time: " + now.toString());
+    startTime = null;
+  }
+}, 1000);
+
+setTimeout(() => clearInterval(interval), second * 60 * 10);
+
+fastify.post(
+  "/place-order",
+  {
+    schema: {
+      body: schema,
+    },
+  },
+  async function handler(request, _) {
+    observable.next(request.body);
+    if (!startTime) startTime = new Date();
+  },
+);
+
+try {
+  const port = 3000;
+  fastify.listen({ port: port }, function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    } else console.log(`Now listening on port ${address}`);
+  });
+} catch (e: any) {
+  console.error(e);
+}
